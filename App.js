bd0df498dc36cbc65fedbe7659a100e2ff53e176
Zaydee <zaydominguez8223@gmail.com>
2023-11-29 16:39:05 -0500

    adding all dependencies - update app + add screens

--- App.js
+++ App.js
@@ -1,6 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
+//importing navigation container and navigation stack
+
+//nav container
+import { NavigationContainer } from '@react-navigation/native';
+
+//nav stack
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+
+//import two screens needed
+import HomeScreen from './screens/HomeScreen';
+import DetailScreen from './screens/DetailScreen';
+
+//create the stack for navigation
+const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <View style={styles.container}>
